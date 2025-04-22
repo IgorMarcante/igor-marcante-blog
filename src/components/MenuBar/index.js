0dedd1c6d908af5bfc4ce9bf2255e7449583804d
Igor Marcante <igormarcante@hotmail.com>
2025-04-21 22:10:32 -0400

    correcoes

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,39 +1,62 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
  
 import { FaHome as Home} from "react-icons/fa";
  import { FaSearch as Search} from "react-icons/fa";
  import { FaArrowUp as Arrow} from "react-icons/fa";
  import { FaRegLightbulb as Light} from "react-icons/fa";
  import { BsFillGrid3X3GapFill as Grid} from "react-icons/bs";
+ import { FaList as List  } from "react-icons/fa";
+ import getThemeColor from '../../utils/getThemeColor'
 
  import * as S from "./styled"
- 
- const MenuBar = () => (
-   <S.MenuBarWrapper>
-     <S.MenuBarGroup>
-       <S.MenuBarLink to="/" title="Voltar para Home">
-         <S.MenuBarItem>
-           <Home />
-         </S.MenuBarItem>
-       </S.MenuBarLink>
-       <S.MenuBarLink to="/search/" title="Pesquisar">
-         <S.MenuBarItem>
-           <Search />
-         </S.MenuBarItem>
-       </S.MenuBarLink>
-     </S.MenuBarGroup>
-     <S.MenuBarGroup>
-       <S.MenuBarItem title="Mudar o tema">
-         <Light />
-       </S.MenuBarItem>
-       <S.MenuBarItem title="Mudar visualização">
-         <Grid />
-       </S.MenuBarItem>
-       <S.MenuBarItem title="Ir para o Topo">
-         <Arrow />
-       </S.MenuBarItem>
-     </S.MenuBarGroup>
-   </S.MenuBarWrapper>
- )
- 
- export default MenuBar
+ const MenuBar = () => {
+    const [theme, setTheme] = useState(null)
+    const [display, setDisplay] = useState(null)
+  
+    const isDarkMode = theme === "dark"
+    const isListMode = display === "list"
+  
+    useEffect(() => {
+      setTheme(window.__theme)
+      setDisplay(window.__display)
+      window.__onThemeChange = () => setTheme(window.__theme)
+      window.__onDisplayChange = () => setDisplay(window.__display)
+    }, []);
+  
+    return (
+      <S.MenuBarWrapper>
+        <S.MenuBarGroup>
+          <S.MenuBarLink to="/" cover direction="right" bg={getThemeColor()} duration={0.6} title="Voltar para Home">
+            <S.MenuBarItem><Home /></S.MenuBarItem>
+          </S.MenuBarLink>
+          <S.MenuBarLink to="/search/" cover direction="right" bg={getThemeColor()} duration={0.6} title="Pesquisar">
+            <S.MenuBarItem><Search /></S.MenuBarItem>
+          </S.MenuBarLink>
+        </S.MenuBarGroup>
+        <S.MenuBarGroup>
+          <S.MenuBarItem
+            title="Mudar o tema"
+            onClick={() => {
+              window.__setPreferredTheme(isDarkMode ? "light" : "dark")
+            }}
+            className={theme}
+          ><Light />
+          </S.MenuBarItem>
+          <S.MenuBarItem
+            title="Mudar visualização"
+            onClick={() => {
+              window.__setPreferredDisplay(isListMode ? "grid" : "list")
+            }}
+            className="display"
+          >
+            {isListMode ? <Grid /> : <List />}
+          </S.MenuBarItem>
+          <S.MenuBarItem title="Ir para o Topo" onClick={() => {
+              window.scroll({ top: 0, behavior: 'smooth' })
+            }}><Arrow /></S.MenuBarItem>
+        </S.MenuBarGroup>
+      </S.MenuBarWrapper>
+    )
+  }
+  
+  export default MenuBar; 
